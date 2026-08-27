eead79c4f45f6557ca8b09ddd692c4e70bb35af9
Althaf-vt <althaafvt@gmail.com>
2026-08-27 12:36:16 +0530

    feat(auth): integrate AuthLayout into RegisterPage workflow

--- frontend/src/features/auth/pages/RegisterPage.tsx
+++ frontend/src/features/auth/pages/RegisterPage.tsx
@@ -1,19 +1,15 @@
 import { RegisterForm } from "../components/RegisterForm";
 import { OtpVerification } from "../components/OtpVerification";
 import { useAppSelector } from "../../../app/hooks";
+import { AuthLayout } from "../../../shared/components/layouts/AuthLayout";
 
 export const RegisterPage = () => {
     const currentStep = useAppSelector((state: any) => state.auth.currentStep);
 
     return (
-        <div>
-            {/* 
-                This is where we need to place our Figma layout wappers,
-                split screens, or branding assets.
-            */}
-
+        <AuthLayout>
             {currentStep === "REGISTER" && <RegisterForm/>}
             {currentStep === "OTP" && <OtpVerification/>}
-        </div>
+        </AuthLayout>
     )
 }
