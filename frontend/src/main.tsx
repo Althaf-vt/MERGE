e14bd254a322aa99d882292a68a6a7d6a77a3fdc
Althaf-vt <althaafvt@gmail.com>
2026-09-11 09:19:08 +0530

    build(frontend): add react-oauth provider to application root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDom from 'react-dom/client';
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { App } from "./app";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDom.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         {/* The provider gives every component inside <App/>  access to Redux and RTK Query*/}
         <Provider store={store}>
-            <App/>
+            <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ''}>
+                <App/>
+            </GoogleOAuthProvider>
         </Provider>
     </React.StrictMode>
 )
