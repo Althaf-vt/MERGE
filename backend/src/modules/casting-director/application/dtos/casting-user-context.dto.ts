5edcbe0ba158506ff2bf52e72e42084716ab038a
Althaf-vt <althaafvt@gmail.com>
2026-09-22 23:15:17 +0530

    refactor(casting): remove duplicate facade interface and dto

--- backend/src/modules/casting-director/application/dtos/casting-user-context.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface CastingUserContextDto{
-    id: string;
-    displayName: string;
-    bio: string;
-    relationshipGoal: string;
-    selectedTraits: string[];
-    interests: string[];
-    partnerExpectations?: string;
-}
--- backend/src/modules/casting-director/application/interfaces/casting-user-facade.interface.ts
+++
@@ -1,8 +0,0 @@
-import { CastingUserContextDto } from "../dtos/casting-user-context.dto";
-
-export const CASTING_USER_FACADE = 'CASTING_USER_FACADE';
-
-export interface IcastingUserFacade {
-    getUserContextForCasting(userId: string): Promise<CastingUserContextDto | null>;
-    markCastingComplete(userId: string, personalityVector: number[]): Promise<void>;
-}
